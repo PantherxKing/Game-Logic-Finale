3d58ce397d7dff0e3ab7ac5d8ffc39d46a26b98a
PantherxKing <91898116+PantherxKing@users.noreply.github.com>
2023-04-04 14:58:07 -0400

    IAP & Ads

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,16 +8,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int tasksLeft = 2;
-    public int CrewMatesLeft = 0;
+    public int CrewMatesLeft = 4;
+    public int Hearts = 0;
     public TextMeshProUGUI TasksText;
     public TextMeshProUGUI CrewMatesLeftText;
+    public TextMeshProUGUI HeartsLeft;
     public GameObject GameOverPanel;
     public GameObject YouWinPanel;
+    public GameObject AdButtonPanel;
+
     // Start is called before the first frame update
     void Start()
     {
         GameOverPanel.SetActive(false);
         YouWinPanel.SetActive(false);
+        AdButtonPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,6 +30,7 @@ public class GameManager : MonoBehaviour
     {
         TasksText.text = ($"Tasks Left: {tasksLeft}");
         CrewMatesLeftText.text = ($"Crew Mates Left: {CrewMatesLeft}");
+        HeartsLeft.text = Hearts.ToString();
 
         if (tasksLeft <= 0)
         {
@@ -35,5 +41,27 @@ public class GameManager : MonoBehaviour
         {
             YouWinPanel.SetActive(true);
         }
+    }
+
+    public void lives(int amount)
+    {
+        if (Hearts <= 0)
+        {
+            AdButtonPanel.SetActive(true);
+        }
+        else
+        {
+            Hearts -= amount;
+        }
+    }
+
+    public void No()
+    {
+        AdButtonPanel.SetActive(false);
+    }
+
+    public void Addlives(int amount)
+    {
+        Hearts += amount;
     }
 }
