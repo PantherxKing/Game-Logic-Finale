86603d18045a16d23b37a1217ce7c71fb9270259
PantherxKing <91898116+PantherxKing@users.noreply.github.com>
2023-04-13 12:50:52 -0400

    idk

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -5,34 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
 {
-    public GameObject pauseMenu;
-
-    private void Start()
-    {
-        pauseMenu.SetActive(false);
-    }
-    public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-    }
-    public void ResumeGame()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-    }
     public void SceneSelect(string SceneName)
     {
         SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
     }
-
-    public void StartGame()
-    {
-        SceneManager.LoadScene("SkeldMap");
-    }
-
     public void Quit()
     {
         Application.Quit();
