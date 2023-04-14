74075452ad6c0541c6b5a463520b2b46fec7d1fe
Malkito <91899165+Malkito@users.noreply.github.com>
2023-04-14 19:35:40 -0400

    Analytics part 2, electric bogaloo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using Unity.Services.Analytics;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,6 +21,9 @@ public class GameManager : MonoBehaviour
     public GameObject YouWinPanel;
     public GameObject AdButtonPanel;
     public GameObject gs;
+    public int wins;
+    public int lose;
+
 
     // Start is called before the first frame update
     void Start()
@@ -45,12 +49,15 @@ public class GameManager : MonoBehaviour
         if(CrewMatesLeft <= 0)
         {
             YouWinPanel.SetActive(true);
+            wins += 1;
+            gameWonEvent();
         }
     }
 
     public void GameOver()
     {
         GameOverPanel.SetActive(true);
+        gameLoseEvent();
         Time.timeScale = 0f;
     }
 
@@ -89,4 +96,42 @@ public class GameManager : MonoBehaviour
     {
         Hearts += amount;
     }
+
+
+    public void gameWonEvent()
+    {
+        //Define Custom Parameters
+        Dictionary<string, object> parameters = new Dictionary<string, object>()
+        {
+           { "wonGameParam", wins}
+        };
+
+        // The ?levelCompleted? event will get cached locally 
+        //and sent during the next scheduled upload, within 1 minute
+        AnalyticsService.Instance.CustomData("wonGame", parameters);
+        // You can call Events.Flush() to send the event immediately
+        AnalyticsService.Instance.Flush();
+    }
+
+    public void gameLoseEvent()
+    {
+        //Define Custom Parameters
+        Dictionary<string, object> parameters = new Dictionary<string, object>()
+        {
+           { "loseGameParam", lose}
+        };
+
+        // The ?levelCompleted? event will get cached locally 
+        //and sent during the next scheduled upload, within 1 minute
+        AnalyticsService.Instance.CustomData("loseGame", parameters);
+        // You can call Events.Flush() to send the event immediately
+        AnalyticsService.Instance.Flush();
+    }
+
+
+
+
+
+
+
 }
